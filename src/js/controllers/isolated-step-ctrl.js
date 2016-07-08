184eed7f57e053831bf00494ff59f0f9de920082
nancy <nancy@nancys-MacBook-Air.local>
2016-07-07 23:22:51 -0600

    init checkin dash+multistepform

---
+++ src/js/controllers/isolated-step-ctrl.js
@@ -0,0 +1,12 @@
+angular.module('RDash').controller('IsolatedStepCtrl', [
+    '$scope',
+    'multiStepFormScope',
+    function ($scope, multiStepFormScope) {
+        $scope.model = angular.copy(multiStepFormScope.model);
+        $scope.$on('$destroy', function () {
+            multiStepFormScope.model = angular.copy($scope.model);
+        });
+    }
+]);
+
+
---
+++ src/js/controllers/session-ctrl.js
@@ -0,0 +1,34 @@
+/**
+ * Session Controller
+ */
+
+angular.module('RDash')
+        .controller('SessionCtrl', ['$scope', SessionCtrl]);
+
+function SessionCtrl($scope) {
+    $scope.model = {};
+    $scope.steps = [
+        {
+            templateUrl: '/templates/multi-step-form/step1.html',
+            title: 'Saving data'
+        },
+        {
+            templateUrl: '/templates/multi-step-form/step2.html',
+            hasForm: true,
+            title: 'Using scope inheritence'
+        },
+        {
+            templateUrl: '/templates/multi-step-form/step3.html',
+            hasForm: true,
+            isolatedScope: true,
+            controller: 'IsolatedStepCtrl',
+            title: 'Isolated step scopes'
+        },
+        {
+            templateUrl: '/templates/multi-step-form/step4.html',
+            title: 'name is...'
+        }
+    ];
+}
+
+
