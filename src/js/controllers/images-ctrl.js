a4f7d098d615867e3ffd9d5f9b4fd87579c9fd02
nancy <nancy@nancys-MacBook-Air.local>
2016-10-21 14:22:22 -0600

    get file content

--- src/js/controllers/images-ctrl.js
+++ src/js/controllers/images-ctrl.js
@@ -1,6 +1,7 @@
 angular.module('RDash')
         .controller('ImagesCtrl', ['$scope',
             function ($scope) {
+             
                 $scope.images = ["img/gallery/1.jpg", "img/gallery/2.jpg", "img/gallery/3.jpg", "img/gallery/4.jpg",
                     "img/gallery/5.jpg", "img/gallery/6.jpg", "img/gallery/7.jpg", "img/gallery/8.jpg",
                     "img/gallery/9.jpg", "img/gallery/10.jpg", "img/gallery/11.jpg", "img/gallery/12.jpg",
@@ -15,12 +16,54 @@ angular.module('RDash')
                 };
 
                 $scope.showContent = function ($file) {
-                     console.log("in ShowContent" + $file.file);
+                    console.log("in ShowContent" + $file.file);
                     console.log("in ShowContent" + JSON.stringify($file.fileContent));
-                    
-                        $scope.images.push($file.fileContent);
+
+                    $scope.images.push($file.fileContent);
 
                 };
+
+                var getFileBlob = function (url, cb) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", url);
+                    xhr.responseType = "blob";
+                    xhr.addEventListener('load', function () {
+                        cb(xhr.response);
+                    });
+                    xhr.send();
+                };
+
+                var blobToFile = function (blob, name) {
+                    blob.lastModifiedDate = new Date();
+                    blob.name = name;
+                    return blob;
+                };
+
+                var getFileObject = function (filePathOrUrl, cb) {
+                    getFileBlob(filePathOrUrl, function (blob) {
+                        cb(blobToFile(blob, 'test.jpg'));
+                    });
+                };
+                /*
+                for (var i = 0; i < imagePaths.length; i++) {
+                    
+                    getFileObject(imagePaths[i], function (fileObject) {
+                        console.log("getFileObject" + i);
+                        var reader = new FileReader();
+
+                        reader.onload = function (onLoadEvent) {
+                            var content = onLoadEvent.target.result;
+                           // $scope.images.push(content);
+                            //console.log("in onload" + JSON.stringify(content));
+
+                        };
+
+                        reader.readAsDataURL(fileObject);
+                    });
+                }*/
+
+
+
 
             }]);
 
--- src/js/directives/filebutton.js
+++ src/js/directives/filebutton.js
@@ -9,13 +9,13 @@ angular
             var fn = $parse(attrs.onReadFile);
 
             element.on('change', function (onChangeEvent) {
-                console.log("on change "+(onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+               // console.log("on change "+(onChangeEvent.srcElement || onChangeEvent.target).files[0]);
                 var reader = new FileReader();
 
                 reader.onload = function (onLoadEvent) {
-                    console.log("on load "+(onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+                 //   console.log("on load "+(onChangeEvent.srcElement || onChangeEvent.target).files[0]);
                     scope.$apply(function () {
-                        console.log("in apply "+(onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+                  //      console.log("in apply "+(onChangeEvent.srcElement || onChangeEvent.target).files[0]);
                         fn(scope, {$file: {file: (onChangeEvent.srcElement || onChangeEvent.target).files[0],
                             fileContent: onLoadEvent.target.result}});
                     });
